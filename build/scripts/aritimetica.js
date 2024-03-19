a0c6db878d88ae6afce3a21606a3ca3499bb81da
Bayer.py <128390959+bayerpy@users.noreply.github.com>
2024-03-19 11:45:09 -0300

    Projeto Gulp

---
+++ build/scripts/aritimetica.js
@@ -0,0 +1,1 @@
+const somar=require("./sum");console.log("Somar: ",somar(7,14));
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require ('gulp-uglify')
+const imagemin = require('gulp-imagemin');
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "compressed"
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
+}
+
+function comprimeImagens() {
+    return gulp.src('./source/png/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/png'));
+}
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
+} 
+
+exports.sass = compilaSass;
+exports.javascript = comprimeJavaScript;
+exports.images = comprimeImagens;
